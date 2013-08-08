2f2a937c2877c69a5be3279782b8f0c0cb0b6430
Dan Tao <daniel.tao@gmail.com>
2013-08-07 23:14:01 -0700

    InstaPie 2?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstapieRails::Application.config.secret_key_base = '57628a176ede360d90abb596e0ba8cffbc45fdadc660bccfc573d66fd3e2e056a096fea161fb4c1b96fcee43696c8686889b245f0cfe2dd2cc1a28e40da644ef'
