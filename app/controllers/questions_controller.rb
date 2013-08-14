bbed3226261e6238091986e17a0fc52d541ccf95
Dan Tao <daniel.tao@gmail.com>
2013-08-14 09:08:05 -0700

    now you can ask a question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,30 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    question = current_user.ask_question(*ask_question_params)
+    flash[:notice] = "Asked question \"#{question.question}\""
+    redirect_to(question)
+  end
+
+  private
+
+  def ask_question_params
+    question_params = params.require(:question).permit(:question, :description)
+
+    [
+      question_params[:question],
+      question_params[:description],
+      options_params
+    ]
+  end
+
+  def options_params
+    params.require(:options).map { |option| option[:option] }
+  end
 end
